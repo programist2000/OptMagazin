a3a5a4a3ed43fc8721c08a5efa62c0c5db09f1ca
privet <privet@PRIVET->
2017-06-04 14:59:17 +0500

    Добавьте файлы проекта.

---
+++ OptMagazin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OptMagazin.MvcApplication" Language="C#" %>
---
+++ OptMagazin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OptMagazin.Startup))]
+namespace OptMagazin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
